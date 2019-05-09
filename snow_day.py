c78616a0d87c5d010cf7753be44d27709a7c135c
Eric Wu <ewu@cpsd.us>
2019-05-09 16:58:27 -0400

    updated snow day

--- snow_day.py
+++ snow_day.py
@@ -26,10 +26,10 @@ for sheet in sheet_list:
     # Get course ID
     course_id = read_course_id(SPREADSHEET_ID, sheet, service_sheets)
     print("In snow day, currently doing course with this ID: {}".format(course_id))
-    values = read_course_daily_data_all(SPREADSHEET_ID, sheet, service_sheets)
-    print("values")
-    print(values)
-    for i, row in enumerate(values, 1):
+    orig_values = read_course_daily_data_all(SPREADSHEET_ID, sheet, service_sheets)
+    print("orig+values")
+    print(orig_values)
+    for i, row in enumerate(orig_values, 1):
         if i > 180:
             break
         if len(row) == 7:
@@ -73,21 +73,60 @@ for sheet in sheet_list:
                 continue
             if i > 179:
                 break
-            if row[1] == immovable_day_data[1]:
+            if row[1] == immovable_day_data[1]:  # matching date
                 print("TRYING!")
-                print('row ' + str(row))
+                print('rowmatch ' + str(row))
+                print("i in row match " + str(i))
+                print('values [i]' + str(values[i]))
                 print('immovable data ' + str(immovable_day_data))
                 print(int(immovable_day_data[0]) - 1)
-                if row[1] == immovable_day_data[1]:
+                if row[5] == immovable_day_data[5] and len(row[5]) >= 6:
                     print("already in the right spot pass")
                     pass
                 else:
-                    print("row is " + str(row))
-                    print('unmovable day is ' + str(immovable_day_data))
-                    temp_data = row
-                    new_values[int(immovable_day_data[0]) - 1] = immovable_day_data
-                    new_values[i] = row
+                    for j, row2 in enumerate(values, 0):
+                        print("row2 is " + str(row2))
+                        print('unmovable day is ' + str(immovable_day_data))
+                        if len(row2) == 7:
+                            if immovable_day_data[3] == row2[3]:
+                                print("row switching is beginning!")
+                                print('row2 to swap ' + str(row2))
+                                print("row to swap " + str(row))
+                                print("index that is immovable " + str(int(immovable_day_data[0]) - 1))
+                                print('index to swap' + str(i))
+                                print('new values [i]' + str(new_values[i]))
+                                print('new values [j]' + str(new_values[j]))
+
+                                temp_values = new_values[i]
+                                print("temp values " + str(temp_values))
+                                print("new values [j][3:] " + str(new_values[j][3:]))
+                                print("temp_values[3:]" + str(temp_values[3:]))
+
+                                if len(new_values[i]) == len(new_values[j]):
+                                    new_values[i][3:] = new_values[j][3:]
+                                    new_values[j][3:] = temp_values[3:]
+                                else:
+                                    # zero place where it's going
+                                    for item in new_values[i]:
+                                        item = 0
+                                    print("swapping out i")
+                                    for counter, item in enumerate(new_values[j], 0):
+                                        print(new_values[j][counter])
+                                        new_values[i][counter] = new_values[j][counter]
+                                    for item in new_values[j]:
+                                        item = 0
+                                    print('swapping out j')
+                                    for counter, item in enumerate(new_values[i], 0):
+                                        print(temp_values[counter])
+                                        new_values[j][counter] = temp_values[counter]
+
+                                print('after')
+                                print('new values [i]' + str(new_values[i]))
+                                print('new values [j]' + str(new_values[j]))
+
+                                break
                     break
-        print(new_values)
+        for new_value in new_values:
+            print(new_value)
         all_sheets.append(new_values)
         sheet_counter += 1
