34641bec7130a5513db94dd69e866f12146d4660
Eric Wu <ewu@cpsd.us>
2019-05-10 14:34:54 -0400

    added delete_announcements that actually works

--- delete_all_announcements_assignments.py
+++ delete_all_announcements_assignments.py
@@ -2,12 +2,23 @@
 from generate_sheets_credential import generate_sheets_credential
 from generate_classroom_credential import generate_classroom_credential
 
-course_id = 36577460997
+course_id = 36642186002
 service_classroom = generate_classroom_credential()
-assignments = service_classroom.courses().courseWork().list(courseId=course_id,).execute()
+assignments = service_classroom.courses().courseWork().list(courseId=course_id,
+                                                            courseWorkStates='DRAFT').execute()
 assignments_list = assignments['courseWork']
 
 print(assignments_list)
 for assignment in assignments_list:
     print(assignment['id'])
     deletion = service_classroom.courses().courseWork().delete(courseId=course_id, id=assignment['id']).execute()
+
+announcements = service_classroom.courses().announcements().list(courseId=course_id,
+                                                                 announcementStates='DRAFT').execute()
+print(announcements)
+announcements_list = announcements['announcements']
+
+print(announcements_list)
+for announcements in announcements_list:
+    print(announcements['id'])
+    deletion = service_classroom.courses().announcements().delete(courseId=course_id, id=announcements['id']).execute()
