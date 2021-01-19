767d9ccead6fb18a31bd77ff884812201eba7e20
ewu <ewu@cpsd.us>
2021-01-19 12:50:03 -0500

    udpated

---
+++ create_assignments_announcements_2021_Murphy_ICS.py
@@ -0,0 +1,3 @@
+from create_assignments_announcements import create_assignments_announcements
+SPREADSHEET_ID= '1o_YPtSYB75fk9-r79GhNVqTDdkWr0VZA497nrC4r1h0'
+create_assignments_announcements(SPREADSHEET_ID)
