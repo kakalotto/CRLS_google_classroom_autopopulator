e5f02c561e6e3a9b8ee8104e9ac87c433f3fc6db
kakalotto <ewu@cpsd.us>
2021-01-19 11:01:38 -0500

    to pull

---
+++ create_assignments_announcements_testing.py
@@ -0,0 +1,3 @@
+from create_assignments_announcements import create_assignments_announcements
+SPREADSHEET_ID= '1HBh9DcDUaKiyH_X5w_-qCh-Aw_0JZD-Rtzg_PfiER3Y'
+create_assignments_announcements(SPREADSHEET_ID)
