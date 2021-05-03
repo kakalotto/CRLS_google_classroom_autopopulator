cdaea1b08e115dee7421122387e21448df510c19
kakalotto <ewu@cpsd.us>
2021-05-03 15:44:37 -0400

    Updated stuffS

--- dr_lam_document_2021_ICS_S2_P1.py
+++ dr_lam_document_2021_ICS_S2_P1.py
@@ -1,4 +1,4 @@
-from dr_lam_document import dr_lam_document
+from dr_lam_document2 import dr_lam_document_2
 
 DOCUMENT_ID = '12BgUzPEsYC3Z8EuJ9aQEStRw5BSgX-EeowEcPJ3sJ-A'
 CLASSROOM_ID = 'MjQ2OTY4NDI4Mzcw'
@@ -31,6 +31,6 @@ assignments_dictionary = {
 }
 
 
-dr_lam_document(document_id=DOCUMENT_ID, classroom_id=CLASSROOM_ID, spreadsheet_id=SPREADSHEET_ID, sheet_id=SHEET_ID,
+dr_lam_document_2(document_id=DOCUMENT_ID, classroom_id=CLASSROOM_ID, spreadsheet_id=SPREADSHEET_ID, sheet_id=SHEET_ID,
                 header_text=HEADER_TEXT, course_id=COURSE_ID, zoom_links=ZOOM_LINKS,
                 assignments_dictionary=assignments_dictionary, course_contract_link=COURSE_CONTRACT)
