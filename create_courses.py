55714d22941e154ccfec06c06226b31cf194df23
Eric Wu <EWu@cpsd.us>
2021-05-14 15:08:49 -0400

    updated create courses

--- create_courses.py
+++ create_courses.py
@@ -1,25 +1,20 @@
-import googleapiclient
+import googleapiclient.errors
+import configparser
 
 from generate_sheets_credential import generate_sheets_credential
 from generate_classroom_credential import generate_classroom_credential
 
-print("Running create_courses.py")
-# Set up sheets service object
-service_sheets = generate_sheets_credential()
-
-#SPREADSHEET_ID = '1uEmlRJjqEpsCj6wew44meJv3af0WQ7VRQFE4x-QU9FQ' # AP CSP test
-#SPREADSHEET_ID = '1RFSXj_IjfqVLv-njFmeI1e0LoVdglvFjQGxk1drxdqw'  # Game development
-#SPREADSHEET_ID = '1o_YPtSYB75fk9-r79GhNVqTDdkWr0VZA497nrC4r1h0' # ICS 2021
-#SPREADSHEET_ID = '1HqwVlxXu-l1KCatU8lnYb9xD97djeA3BkO448K5HADk' # testing
-#SPREADSHEET_ID = '1x0buDsw6pBjK1GJkZXc5mMo86dX7FnWj4daMn_5CyLc' # 2021 IT2/IT3
-SPREADSHEET_ID = '1ZenTcQlCQhbYvBvPOVq8XIB2FQgseIGHH4gTBTcw-KY' # 2021 level 1
-#SPREADSHEET_ID = '1xkcNN1OFmscODqz3zbDUqRbkHAxIuIyx-FtMfXgqczA' # AP CSP test
-#SPREADSHEET_ID = '1RFSXj_IjfqVLv-njFmeI1e0LoVdglvFjQGxk1drxdqw'  # Game development
-#SPREADSHEET_ID = '1NmV9WVSJsVrrJIVRKat4oWnkWMBWCEuO-07eCesh4ow' # 2020 level 1
-#SPREADSHEET_ID = '1HBh9DcDUaKiyH_X5w_-qCh-Aw_0JZD-Rtzg_PfiER3Y' # 2020 level 1
-
 SHEET_NAME = 'Courses'
 
+print("Running create_courses.py")
+config = configparser.ConfigParser()
+config_filename = "google_classroom_tools.ini"
+config.read(config_filename)
+spreadsheet_id = config.get("CREATE_COURSES", "spreadsheet_id", fallback='')
+print("Trying to read Google sheet with this spreadsheet ID: " + str(spreadsheet_id))
+
+# Set up sheets service object
+service_sheets = generate_sheets_credential()
 
 # Set up classroom service object
 service_classroom = generate_classroom_credential()
@@ -29,7 +24,7 @@ for column in ['E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']:
     RANGE_NAME = SHEET_NAME + '!' + column + '3:' + column + '10'
 
     # Read course info
-    result = service_sheets.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
+    result = service_sheets.spreadsheets().values().get(spreadsheetId=spreadsheet_id,
                                                         range=RANGE_NAME,
                                                         majorDimension='COLUMNS').execute()
     value = result.get('values', [])
@@ -61,16 +56,16 @@ for column in ['E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']:
         body = {
             'values': values
         }
-        RANGE_NAME = SHEET_NAME + '!' + column + '9'
+        range_name = SHEET_NAME + '!' + column + '9'
         try:
-            result = service_sheets.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME,
-                                                               valueInputOption='USER_ENTERED', body=body).execute()
+            result = service_sheets.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=range_name,
+                                                                   valueInputOption='USER_ENTERED', body=body).execute()
             print('{} cells in Google sheet with spreadsheetID {} updated with courseID {}.'
-                  .format(result.get('updatedCells'), SPREADSHEET_ID, course.get('id')))
+                  .format(result.get('updatedCells'), spreadsheet_id, course.get('id')))
         except googleapiclient.errors.HttpError:
             raise Exception("Possible errors.\n  Did you put in the correct spreadsheet?  Spreadsheet should be "
                             "something like '1xkcNN1OFmscODqz3zbDUqRbkHAxIuIyx-FtMfXgqczA'\n"
-                            "Spreadsheet actually is {}.".format(SPREADSHEET_ID))
+                            "Spreadsheet actually is {}.".format(spreadsheet_id))
         # Topics
         topics = value[0][5].split(',')
         course_id = course.get('id')
@@ -84,6 +79,5 @@ for column in ['E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']:
             except googleapiclient.errors.HttpError:
                 raise Exception("Possible errors.\n  If 'requested entity already exists', maybe already have topic {}."
                                 "\n If 'requested entity not found', then the course id {} may not exist.\n)."
-                    .format(topic, values))
-
+                                .format(topic, values))
 
