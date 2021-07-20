9c8d4467f826c34d1979189c7a8f9745da1716b3
ewu <ewu@cpsd.us>
2021-07-19 20:20:46 -0400

    updated for level1

--- post_everything.py
+++ post_everything.py
@@ -3,7 +3,7 @@ import datetime
 from generate_sheets_credential import generate_sheets_credential
 from generate_classroom_credential import generate_classroom_credential
 
-course_id = 247697843639
+course_id = 371236480803
 service_classroom = generate_classroom_credential()
 
 assignments = service_classroom.courses().courseWork().list(courseId=course_id,
