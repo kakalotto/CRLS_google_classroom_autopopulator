2908083d383a64f4bab6ef6302530afa5cd7d562
ewu <ewu@cpsd.us>
2021-07-19 20:20:02 -0400

    updated for  level1 2021-2022

--- create_dr_lam_document_config.py
+++ create_dr_lam_document_config.py
@@ -21,13 +21,13 @@ it2_2021_fy_p3 = {'document_id': '1BXeTkfvxq4qMKOWT2Gshkhg7RbzgSjsW_OWUHCkBqDs',
                   'assignments_dictionary': {},
                   'fy': True
                   }
-it1_2021_fy_p1 = {'document_id': '1L3R-VH_M0VJKWnOZ9GqBEJkYuhwUiQDgT6897LdfY68',
-                  'classroom_id': 'MTY0OTc4MTI5Mzg4',
-                  'spreadsheet_id': '1ZenTcQlCQhbYvBvPOVq8XIB2FQgseIGHH4gTBTcw-KY',
+it1_2022_fy_p1 = {'document_id': '14uKkNw6tHo8foESJsgNcLpKNtjYcEkOhQ8ZDfdC4iRI',
+                  'classroom_id': 'MzcxMjM2NDgwODAz',
+                  'spreadsheet_id': '1PgRLC-fUnUwskXp4h-1Mxf7exF_6KLhnUfqyMgs7aOI',
                   'sheet_id': 'IT1/CS1',
                   'header_text': 'Level 1 Intro to CS/Cybersecurity\n',
-                  'course_id': 164978129388,
-                  'course_contract': 'https://docs.google.com/document/d/1OZ7yTwupKQPZxNjwfF_RN02h82LfpuNwa3HNrvOXX4o',
+                  'course_id': 371236480803,
+                  'course_contract': 'https://docs.google.com/document/d/1zJArrh4Xq0YlF-vFZu3SCYznJhjMpM9pBs2GOLSsJV0/edit',
                   'zoom_links': ['https://zoom.us/j/9332367963?pwd=WElmWmc0dHBqSjY2MDFpaWJsbEFsdz09'],
                   'assignments_dictionary': {},
                   'fy': True
@@ -99,5 +99,5 @@ apcsp_2021_s2_p4 = {'document_id': '1miz7EgIEna8W5fn8wJPqn0U3TL2i49tTORKTOaGoLQs
                     'fy': False
                     }
 
-classes = [ics_2021_s2_p1, it2_2021_fy_p3, it1_2021_fy_p1, apcsp_2021_s2_p1, apcsp_2021_s2_p4]
+classes = [it1_2022_fy_p1]
 
