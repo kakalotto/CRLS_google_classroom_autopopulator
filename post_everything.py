6876034a91674463579a811e9de4d7eda5a7f78b
Eric Wu <you@example.com>
2021-09-08 15:56:52 -0400

    updated

--- post_everything.py
+++ post_everything.py
@@ -3,7 +3,7 @@ import datetime
 from generate_sheets_credential import generate_sheets_credential
 from generate_classroom_credential import generate_classroom_credential
 
-course_id = 388900189656
+course_id = 389181339285
 service_classroom = generate_classroom_credential()
 
 assignments = service_classroom.courses().courseWork().list(courseId=course_id,
