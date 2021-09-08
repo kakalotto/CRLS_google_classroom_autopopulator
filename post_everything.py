ee8dd20a24e229231c020c63324f93fb8babce51
Eric Wu <you@example.com>
2021-09-08 10:07:52 -0400

    updated to delete apcsp 2019

--- post_everything.py
+++ post_everything.py
@@ -3,7 +3,7 @@ import datetime
 from generate_sheets_credential import generate_sheets_credential
 from generate_classroom_credential import generate_classroom_credential
 
-course_id = 371236480803
+course_id = 389091727597
 service_classroom = generate_classroom_credential()
 
 assignments = service_classroom.courses().courseWork().list(courseId=course_id,
